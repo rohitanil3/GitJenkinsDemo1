811b01bcb942c0ae54e00a0a0296ee84702b1308
rohitanil3 <55200072+rohitanil3@users.noreply.github.com>
2024-07-10 20:23:35 +0530

    Update JenkisGitDemoApplication.java
    ok

--- src/main/java/com/example/jenkisgitdemo/JenkisGitDemoApplication.java
+++ src/main/java/com/example/jenkisgitdemo/JenkisGitDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class JenkisGitDemoApplication extends SpringBootServletInitializer {
-
+ 
     public static void main(String[] args) {
         SpringApplication.run(JenkisGitDemoApplication.class, args);
     }
